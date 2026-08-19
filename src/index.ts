0cee5bcf303c0259f69b32d0a236f5ec13c6ae75
Digerati Design <cabal@digerati.design>
2026-08-19 17:28:22 +0100

    migrate mc illustration to finsweet starter

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { initMCDebug } from './mc/core/debug';
 import { initMCMotion } from './mc/core/motion';
 import { initMCColourReveal } from './mc/effects/colour-reveal';
+import { initMCIllustration } from './mc/effects/illustration';
 
 initMCMotion();
 initMCDebug();
 initMCColourReveal();
+initMCIllustration();
